1c6d2355ba32d2fb61095fd472077bce8b70a7b6
Altamiro Bruno <altamiro_bruno@yahoo.com.br>
2024-12-10 15:56:00 -0300

    implementando categorias

---
+++ app/src/main/java/com/altamirobruno/myapplication/model/Category.kt
@@ -0,0 +1,6 @@
+package com.altamirobruno.myapplication.model
+
+data class Category(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/altamirobruno/myapplication/view/CategoryAdapter.kt
@@ -0,0 +1,58 @@
+package com.altamirobruno.myapplication.view
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.altamirobruno.myapplication.HomeFragment
+import com.altamirobruno.myapplication.R
+import com.altamirobruno.myapplication.model.Movie
+import com.bumptech.glide.Glide
+
+
+class CategoryAdapter(
+    private val movies: List<Movie>,
+    private val fragment: Fragment
+) : RecyclerView.Adapter<CategoryAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.movie_item, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+
+    }
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movie: Movie) {
+            val movieTitle: TextView = itemView.findViewById(R.id.movie_title)
+            val movieCover: ImageView = itemView.findViewById(R.id.movie_cover)
+            val posterUrl = "https://image.tmdb.org/t/p/original/${movie.poster_path}"
+            movieTitle.text = movie.title
+
+
+            Glide
+                .with(fragment)
+                .load(movie.poster_path)
+                .centerCrop()
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(movieCover);
+
+
+        }
+
+    }
+
+}
